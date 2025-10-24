a7ea97913d3535af7825bd7b9ea079102b0245d2
EDARA ANITHA <185475127+AnithaEdara@users.noreply.github.com>
2025-10-24 17:33:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/hospital-management/', // <--- Add your base path here
   plugins: [react()],
 })
